f9ccb083b7949d2cfcfc60dc0cf0ffea8f7297f2
jangji96 <jangji96@coreintec.com>
2021-05-13 13:00:46 +0900

    useRef 로 특정 DOM 선택하기

--- src/InputSample.js
+++ src/InputSample.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 // function InputSample() {
 //     const [text, setText] = useState('');
@@ -28,34 +28,48 @@ function InputSample() {
         nickname: ''
     });
 
+    const nameInput = useRef();
+
     const { name, nickname } = inputs; // 비구조화 할당을 통해 값 추출
 
     const onChange = (e) => {
         const { value, name } = e.target; // 우선 e.target 에서 name 과 value 를 추출
         console.log(name);
         setInputs({
-          ...inputs, // 기존의 input 객체를 복사한 뒤
-          [name]: value // name 키를 가진 값을 value 로 설정
+            ...inputs, // 기존의 input 객체를 복사한 뒤
+            [name]: value // name 키를 가진 값을 value 로 설정
         });
-      };
-
-      const onReset = () => {
+    };
+    
+    const onReset = () => {
         setInputs({
-          name: '',
-          nickname: '',
+            name: '',
+            nickname: '',
         })
-      };
+        nameInput.current.focus();
+    };
 
     return (
         <div>
-      <input name="name" placeholder="이름" onChange={onChange} value={name} />
-      <input name="nickname" placeholder="닉네임" onChange={onChange} value={nickname}/>
-      <button onClick={onReset}>초기화</button>
-      <div>
-        <b>값: </b>
-        {name} ({nickname})
-      </div>
-    </div>
+            <input
+                name="name"
+                placeholder="이름"
+                onChange={onChange}
+                value={name}
+                ref={nameInput}
+            />
+            <input 
+                name="nickname"
+                placeholder="닉네임"
+                onChange={onChange}
+                value={nickname}
+            />
+            <button onClick={onReset}>초기화</button>
+            <div>
+                <b>값: </b>
+                {name} ({nickname})
+            </div>
+        </div>
     )
 }
 
