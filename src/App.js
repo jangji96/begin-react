270c6c95c00dfa1b468c332e9b836efb973403c4
jangji96 <xxwlgns@naver.com>
2021-05-14 11:22:09 +0900

    배열에 항목 추가하기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 // import Hello from './Hello';
 // import './App.css';
 // import Wrapper from './Wrapper';
 // import Counter from './Counter';
 // import InputSample from './InputSample';
 import UserList from './UserList';
+import CreateUser from './CreateUser'
 
 function App() {
   // const name = 'react'
@@ -14,6 +15,51 @@ function App() {
   //   fontSize: 24, // 기본 단위 px
   //   padding: '1rem' // 다른 단위 사용 시 문자열로 설정
   // }
+  const [inputs, setInputs] = useState({
+    username: '',
+    email: ''
+  });
+  const { username, email } = inputs;
+  const onChange = e => {
+    const { name, value } = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  };
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com'
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com'
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com'
+    }
+  ]);
+
+  const nextId = useRef(4);
+  const onCreate = () => {
+    const user = {
+      id: nextId.current,
+      username,
+      email
+    };
+    // setUsers([...users, user]); spread 연산자 이용
+    setUsers(users.concat(users)); // concat 이용
+    setInputs({
+      username: '',
+      email: ''
+    });
+    nextId.current += 1;
+  };
   return (
     // <div>
     //   <Hello />
@@ -26,7 +72,15 @@ function App() {
     // </Wrapper>
     // <Counter/>
     // <InputSample/>
-    <UserList/>
+    <>
+      <CreateUser
+          username={username}
+          email={email}
+          onChange={onChange}
+          onCreate={onCreate}
+        />
+      <UserList users={users}/>
+    </>
   );
 }
 
