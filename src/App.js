5adf7b9a294f5973988d31c9be3be5b87642b9b3
jangji96 <xxwlgns@naver.com>
2021-05-24 13:26:21 +0900

    Context API 를 사용한 전역 값 관리 - 2 / Immer 를 사용한 더 쉬운 불변성 관리

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import React, {useRef, useState, useMemo, useCallback, useReducer} from 'react';
 // import InputSample from './InputSample';
 import UserList from './UserList';
 import CreateUser from './CreateUser'
-import useInputs from './hooks/useInputs';
+// import useInputs from './hooks/useInputs';
+import produce from 'immer'
 
 function countActiveUsers(users) {
   console.log('활성 사용자 수를 세는중...');
@@ -39,6 +40,7 @@ const initialState = {
     }
   ]
 }
+
 function reducer(state, action) {
   switch (action.type){
     // case 'CHANGE_INPUT':
@@ -50,22 +52,33 @@ function reducer(state, action) {
     //     }
     //   };
     case 'CREATE_USER':
-      return {
-        inputs: initialState.inputs,
-        users: state.users.concat(action.user)
-      };
+      // return {
+      //   inputs: initialState.inputs,
+      //   users: state.users.concat(action.user)
+      // };
+      return produce(state, draft => {
+        draft.users.push(action.user);
+      });
     case 'TOGGLE_USER':
-      return {
-        ...state,
-        users: state.users.map(user =>
-          user.id === action.id ? { ...user, active: !user.active } : user
-        )
-      };
+      // return {
+      //   ...state,
+      //   users: state.users.map(user =>
+      //     user.id === action.id ? { ...user, active: !user.active } : user
+      //   )
+      // };
+      return produce(state, draft => {
+        const user = draft.users.find(user => user.id === action.id);
+        user.active = !user.active;
+      });
     case 'REMOVE_USER':
-      return {
-        ...state,
-        users: state.users.filter(user => user.id !== action.id)
-      };
+      // return {
+      //   ...state,
+      //   users: state.users.filter(user => user.id !== action.id)
+      // };
+      return produce(state, draft =>{
+        const index = draft.users.findIndex(user => user.id === action.id);
+        draft.users.splice(index, 1);
+      });
     default:
       return state ;
   }
@@ -202,13 +215,13 @@ function App() {
   // }, []);
   // const count = useMemo(() => countActiveUsers(users), [users]);
 
-  const [{ username, email }, onChange, onReset] = useInputs({
-    username: '',
-    email: ''
-  });
+  // const [{ username, email }, onChange, onReset] = useInputs({
+  //   username: '',
+  //   email: ''
+  // });
 
   const [state, dispatch] = useReducer(reducer, initialState);
-  const nextId = useRef(4);
+  // const nextId = useRef(4);
 
   const {users} = state;
   // const {username,email} = state.inputs;
@@ -222,18 +235,18 @@ function App() {
   //   });
   // },[]);
 
-  const onCreate = useCallback(() => {
-    dispatch({
-      type: 'CREATE_USER',
-      user: {
-        id: nextId.current,
-        username,
-        email
-      }
-    });
-    onReset();
-    nextId.current += 1;
-  }, [username, email, onReset]);
+  // const onCreate = useCallback(() => {
+  //   dispatch({
+  //     type: 'CREATE_USER',
+  //     user: {
+  //       id: nextId.current,
+  //       username,
+  //       email
+  //     }
+  //   });
+  //   onReset();
+  //   nextId.current += 1;
+  // }, [username, email, onReset]);
 
   // const onToggle = useCallback(id => {
   //   dispatch({
@@ -272,10 +285,10 @@ function App() {
       <UserList users={users} onRemove={onRemove} onToggle={onToggle}/> */}
       <UserDispatch.Provider value={dispatch}>
         <CreateUser
-          username={username}
-          email={email}
-          onChange={onChange}
-          onCreate={onCreate}
+          // username={username}
+          // email={email}
+          // onChange={onChange}
+          // onCreate={onCreate}
           />
         <UserList users={users}
           // onToggle={onToggle}
